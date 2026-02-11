ad750e46b5f727b015fa8e70e0e7b939429c0ac0
Alivu <35950406+Alivu@users.noreply.github.com>
2026-02-12 02:31:11 +0300

    Update app version to 1.9.6 in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker для ЭлектроСметы
-const APP_VERSION = '1.9.5'; // ← МЕНЯЙТЕ ПРИ КАЖДОМ ОБНОВЛЕНИИ ПРАЙСА!
+const APP_VERSION = '1.9.6'; // ← МЕНЯЙТЕ ПРИ КАЖДОМ ОБНОВЛЕНИИ ПРАЙСА!
 const CACHE_NAME = `electro-smeta-${APP_VERSION}`;
 
 // Установка
