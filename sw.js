186b9830e73c40aa1c8279a84a2bc78d54009f0c
Alivu <35950406+Alivu@users.noreply.github.com>
2026-02-13 21:40:29 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker для ЭлектроСметы
-const APP_VERSION = '1.9.8'; // ← МЕНЯЙТЕ ПРИ КАЖДОМ ОБНОВЛЕНИИ ПРАЙСА!
+const APP_VERSION = '1.9.9'; // ← МЕНЯЙТЕ ПРИ КАЖДОМ ОБНОВЛЕНИИ ПРАЙСА!
 const CACHE_NAME = `electro-smeta-${APP_VERSION}`;
 
 // Установка
